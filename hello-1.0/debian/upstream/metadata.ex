015d3c1980f0fb3f410fa5771a47f80662b8441b
Xiaozhan Li <lxz_20081025@163.com>
2024-05-31 14:12:49 +0800

    add hello-1.0.deb

---
+++ hello-1.0/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/hello/issues
+# Bug-Submit: https://github.com/<user>/hello/issues/new
+# Changelog: https://github.com/<user>/hello/blob/master/CHANGES
+# Documentation: https://github.com/<user>/hello/wiki
+# Repository-Browse: https://github.com/<user>/hello
+# Repository: https://github.com/<user>/hello.git
